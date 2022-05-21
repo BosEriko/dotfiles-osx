177897c875a2c4e4bd78d07e84074b21d23e6352
BosEriko <bos.eriko@gmail.com>
2022-05-21 19:10:11 +0800

    add mega folders on create script

--- install/modules/mkdir.sh
+++ install/modules/mkdir.sh
@@ -4,3 +4,6 @@
 mkdir -p ~/Documents/Codes/Work ~/Documents/Codes/Personal
 terminal-notifier -title 'Installing' -message 'Work and Personal folder have been created.'
 
+# Make the directories for Mega
+mkdir -p ~/Music/Mega\ Music ~/Movies/Mega\ Movies
+terminal-notifier -title 'Installing' -message 'Mega folders have been created.'
